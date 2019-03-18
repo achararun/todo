c673c1c57e2c8073c1dc9b6655fc7b02dac1e6db
Arun <achararun@gmail.com>
2019-03-18 21:32:28 +0530

    Version 1.0

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from todo import views as v
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',v.index),
 ]
